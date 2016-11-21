62c825dc1dcf428e5726661a84782428ba436f8c
Bisite_user <bisite_user@MacBook-Air-de-DaniBisite.local>
2016-11-21 11:48:13 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>sample PHP</title><</head>
+<body>
+  <?php
+  $input_data = $_GET['text1'];
+  print('number = '.$input_data. '<br>');
+  print('return num = '.gettype($input_data));
+  ?>
+</body>
+</html>
